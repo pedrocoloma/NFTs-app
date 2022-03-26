1cfbe327a256c4825bdd51f506b938c1cb2c2667
Pedro <pfcaraujo@uolinc.com>
2022-03-26 19:06:16 -0300

    Featured Table View Cell

--- NFTs/Featured/Views/FeaturedCell.swift
+++ NFTs/Featured/Views/FeaturedCell.swift
@@ -13,13 +13,14 @@ class FeaturedCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        selectionStyle = .none
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
     
 }
